d8548c867920ddeb93378b68e76a28def7a843f7
Magdalena260 <magdalena.laurisch@outlook.de>
2026-06-17 12:33:40 +0200

    legal notice....beginning...

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,7 +1,8 @@
 import { Routes } from '@angular/router';
 import { HomepageComponent } from './homepage/homepage';
+import { LegalNoticeComponent } from './legal-notice/legal-notice';
 
 export const routes: Routes = [
-    {path: '', component: HomepageComponent }
+  { path: '', component: HomepageComponent },
+  { path: 'legal-notice', component: LegalNoticeComponent }
 ];
- 
---
+++ src/app/legal-notice/legal-notice.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-legal-notice',
+  standalone: true,
+  templateUrl: './legal-notice.html',
+  styleUrl: './legal-notice.scss'
+})
+export class LegalNoticeComponent {
+
+}
