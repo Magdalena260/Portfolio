96e40be76dbf689b287529f7d46f974277eb65c6
Magdalena260 <magdalena.laurisch@outlook.de>
2026-07-01 11:00:05 +0200

    changes x gone

--- src/app/header/header.ts
+++ src/app/header/header.ts
@@ -10,7 +10,6 @@ import { TranslateService, TranslateModule } from '@ngx-translate/core';
   styleUrl: './header.scss'
 })
 export class HeaderComponent {
-
   currentLang: string = 'en';
   menuOpen = false;
 
@@ -37,30 +36,34 @@ export class HeaderComponent {
   }
 
   scrollTo(id: string) {
-    const element = document.getElementById(id);
-
-    if (element) {
-      const offsets: Record<string, number> = {
-        about: 220,
-        skills: 140,
-        portfolio: 140,
-        references: 260,
-        contact: 140
-      };
-
-      const headerOffset = offsets[id] ?? 140;
-
-      const y =
-        element.getBoundingClientRect().top +
-        window.scrollY -
-        headerOffset;
-
-      window.scrollTo({
-        top: y,
-        behavior: 'smooth'
-      });
-    }
-
     this.menuOpen = false;
+
+    this.router.navigate(['/']).then(() => {
+      setTimeout(() => {
+        const element = document.getElementById(id);
+
+        if (!element) return;
+
+        const offsets: Record<string, number> = {
+          about: 220,
+          skills: 140,
+          portfolio: 140,
+          references: 260,
+          contact: 140
+        };
+
+        const headerOffset = offsets[id] ?? 140;
+
+        const y =
+          element.getBoundingClientRect().top +
+          window.scrollY -
+          headerOffset;
+
+        window.scrollTo({
+          top: y,
+          behavior: 'smooth'
+        });
+      }, 100);
+    });
   }
 }
