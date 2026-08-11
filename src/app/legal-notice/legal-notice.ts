b79b14a8501e70b50c0d1721d2bc921f69e5122a
Magdalena260 <magdalena.laurisch@outlook.de>
2026-08-11 16:23:25 +0200

    legal notice

--- src/app/legal-notice/legal-notice.ts
+++ src/app/legal-notice/legal-notice.ts
@@ -1,10 +1,14 @@
 import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
 import { TranslateModule } from '@ngx-translate/core';
 
 @Component({
   selector: 'app-legal-notice',
   standalone: true,
-  imports: [TranslateModule],
+  imports: [
+    TranslateModule,
+    RouterLink
+  ],
   templateUrl: './legal-notice.html',
   styleUrl: './legal-notice.scss'
 })
